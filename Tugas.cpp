84a8d642758a2f302870510a7cfcbb0865a574d5
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-05-13 18:20:26 +0700

    comit1

---
+++ Tugas.cpp
@@ -0,0 +1,18 @@
+// Buatlah sebuah class bernama bangunDatar yang memiliki member sebagai berikut:
+
+#include <iostream>
+using namespace std;
+
+//deklarasi class Bangundatar
+class bangunDatar;
+
+class PersegiPanjang{
+    public:
+         void inputData(BangunDatar &bd);
+         void outputData(BangunDatar &bd);
+
+};
+
+class BangunDatar{
+    private:
+}
